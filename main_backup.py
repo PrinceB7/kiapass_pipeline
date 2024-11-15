a1c3b328c1a0d087b6833b2fb15b1e323f0d250d
PrinceB7 <bunyod.ibrokhimov@gmail.com>
2024-11-15 23:30:59 +0900

    videos updated

--- main_backup.py
+++ main_backup.py
@@ -1,5 +1,5 @@
 import os
-os.environ["CUDA_VISIBLE_DEVICES"] = '1'
+os.environ["CUDA_VISIBLE_DEVICES"] = '0'
 
 from typing import List
 import cv2
--- segmentation.py
+++ segmentation.py
@@ -1,50 +1,46 @@
 from ultralytics import YOLO
-import torch
 import numpy as np
 from typing import List, Tuple
 
 class SegmentationProcessor:
-    def __init__(self, model_path: str, device: str = 'cuda:1'):
+    def __init__(self, model_path: str, device: str = 'cuda:0'):
         self.device = device
         self.model = YOLO(model_path)
         
-    # def process_batch(self, frames: List[np.ndarray]) -> List[Tuple[np.ndarray, dict]]:
-        
-    #     results = self.model.predict(frames, imgsz=640, device=self.device, 
-    #                                 verbose=False, classes=[0])
-    #     processed = []
-        
-    #     for result in results:
-    #         # Get the original frame
-    #         frame = result.orig_img
-            
-    #         # Draw segmentation masks
-    #         annotated_frame = result.plot(boxes=False, labels=False)
-            
-    #         # Extract metadata
-    #         metadata = {
-    #             'boxes': result.boxes.data.cpu().numpy() if result.boxes else None,
-    #             'masks': result.masks.data.cpu().numpy() if result.masks else None,
-    #             'names': result.names
-    #         }
-            
-    #         processed.append((annotated_frame, metadata))
-            
-    #     return processed
-    
     def process_batch(self, frames: List[np.ndarray]) -> List[Tuple[np.ndarray, dict]]:
         
+        results = self.model.predict(frames, imgsz=640, device=self.device, 
+                                    verbose=False, classes=[0])
         processed = []
         
-        for frame in frames:
-            # Get the original frame
+        for result in results:
+            # Draw segmentation masks
+            annotated_frame = result.plot(boxes=False, labels=False)
             
+            # Extract metadata
             metadata = {
-                'boxes': "boxes",
-                'masks': "masks",
-                'names': "names"
+                'boxes': result.boxes.data.cpu().numpy() if result.boxes else None,
+                'masks': result.masks.data.cpu().numpy() if result.masks else None,
+                'names': result.names
             }
             
-            processed.append((frame, metadata))
+            processed.append((annotated_frame, metadata))
             
         return processed
+    
+    # def process_batch(self, frames: List[np.ndarray]) -> List[Tuple[np.ndarray, dict]]:
+        
+    #     processed = []
+        
+    #     for frame in frames:
+    #         # Get the original frame
+            
+    #         metadata = {
+    #             'boxes': "boxes",
+    #             'masks': "masks",
+    #             'names': "names"
+    #         }
+            
+    #         processed.append((frame, metadata))
+            
+    #     return processed
